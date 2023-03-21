96655bf68e92f04a4f0cdb1b1e79aa80a164dccc
Junfeng-Zhang <60849891+Junfeng-Zhang@users.noreply.github.com>
2023-03-21 16:07:30 +0800

    first commit

---
+++ frontend/src/components/Chat.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="chat" ref="currentMessage">
+    <div class="profile">
+      <img v-if="chat.isAi" src="@/assets/bot.svg" alt="" />
+      <img v-else src="@/assets/user.svg" alt="" />
+    </div>
+    <div class="message">{{ chat.value }}</div>
+  </div>
+</template>
+
+<script setup>
+import { nextTick, onMounted, ref, watch } from "vue";
+defineProps({ // 传出Chat组件属性
+  chat: {
+    type: Object || null,
+  },
+});
+const currentMessage = ref(null);
+onMounted(async () => {
+  await nextTick();
+  const div = currentMessage.value;
+  div.scrollIntoView({ behavior: "smooth" }); // 加载到最新消息条
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <main>
+    <div id="chat_container">
+      <div
+        v-for="(chat, i) in wrapper"
+        :key="i"
+        class="wrapper"
+        :class="{ ai: chat.isAi }"
+      >
+        <Chat :chat="chat" :key="i" />
+      </div>
+    </div>
+    <form @submit.prevent="fetchAnswer">
+      <textarea
+        rows="1"
+        cols="1"
+        placeholder="Type Your Contents..."
+        v-model="question"
+      >
+      </textarea>
+      <button type="submit"><img src="@/assets/send.svg" alt="send" /></button>
+    </form>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Chat from "../components/Chat.vue";
+
+const question = ref("");
+const wrapper = ref([]);
+const loading = ref(false);
+
+const fetchAnswer = async () => {
+  try {
+    loading.value = true;
+    wrapper.value.push({
+      isAi: false,
+      value: question.value,
+    });
+    wrapper.value.push({
+      isAi: true,
+      value: "Loading...",
+    });
+
+    const res = await fetch("http://localhost:8000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        question: question.value,
+      }),
+    });
+    const data = await res.json();
+
+    const parsedData = data.bot.trim(); // 从文本的开头和结尾删除前面的空格
+    wrapper.value.pop(); // 移除加载状态
+    wrapper.value.push({
+      isAi: true,
+      value: parsedData,
+    });
+  } catch (error) {
+  } finally {
+    loading.value;
+    question.value = ""; // 清空输入框
+  }
+};
+</script>
